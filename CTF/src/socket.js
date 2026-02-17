40982f06d01f2c491e3ea7e9264d57ad7427cf63
Harsh <harshkittu10@gmail.com>
2026-02-17 09:48:02 +0530

    Add express dependency

--- CTF/src/socket.js
+++ CTF/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
-  withCredentials: true
+export const socket = io("https://ctf-3ztj.onrender.com", {
+  withCredentials: true,
+  transports: ["websocket"]
 });
